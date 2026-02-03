977910e20f43b9cea959f6548aa67ca1621eb237
DANH PHI LONG <idlongggg@users.noreply.github.com>
2026-02-03 21:37:30 +0700

    feat: update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,20 @@
-import './globals.css';
 import { AuthProvider } from '@/lib/auth';
+import { Baloo_2 } from 'next/font/google';
+import './globals.css';
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <AuthProvider>{children}</AuthProvider>;
+const baloo2 = Baloo_2({
+  subsets: ['latin'],
+  weight: '400',
+  variable: '--font-body',
+  display: 'swap',
+});
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="vi">
+      <body className={`${baloo2.variable}`}>
+        <AuthProvider>{children}</AuthProvider>
+      </body>
+    </html>
+  );
 }
